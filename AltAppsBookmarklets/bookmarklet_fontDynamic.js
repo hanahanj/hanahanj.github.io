2a6ab982f32a168f654ae034e9d7ecaf55e8f4ec
Hanahan <jonathan@milieu.works>
2017-09-13 09:15:43 -0700

    added heading 1 decrease on click

--- AltAppsBookmarklets/bookmarklet_fontDynamic.js
+++ AltAppsBookmarklets/bookmarklet_fontDynamic.js
@@ -51,7 +51,16 @@ function initMyBookmarklet(){
 				} else {
 					var s=12;}s+=10;p[i].style.fontSize=s+"px"}
 
-				});
+			var p=document.getElementsByTagName('h1');
+			for(i=0;i<p.length;i++){
+				if(p[i].style.fontSize){
+					var s=parseInt(p[i].style.fontSize.replace("px",""));
+				} else {
+					var s=12;}s-=2;p[i].style.fontSize=s+"px"}
+
+
+
+		});
 
 
 	})();
